de79a77ea0b5906b270b41761f47ed0d52775e79
ToasterTom <tate.thomas5595@gmail.com>
2016-10-22 12:51:38 -0600

    cors options

--- public/server/server.js
+++ public/server/server.js
@@ -1,4 +1,18 @@
 var express = require("express");
 var app = express();
+var port = 8000
 var bodyParser = require('body-parser');
 var session = require('express-session');
+
+app.use(bodyParser.json());
+app.use(cors());
+
+// Makes it so the request can only come from this port.
+var corsOptions = {
+  origin: 'http://localhost:8000'
+}
+
+
+app.listen(port, function(){
+  console.log("Listeing on port ", port, " All your base belong to us!");
+});
